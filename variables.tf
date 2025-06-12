04e50569091eb4b650af41a68938f711e2cb33c6
pothuguntarakesh <pothugunta.rakesh.com>
2025-06-12 22:56:00 +0530

    dg

--- variables.tf
+++ variables.tf
@@ -1,24 +1,165 @@
-variable "project_name" {
-    type = string 
+# variable "project_name" {
+#     type = string 
+# }
+
+# variable "environment" {
+#     type = string
+#     default = "dev"
+# }
+
+# variable "common_tags" {
+#     type = map
+# }
+
+
+
+# variable "vpc_cidr" {
+#     type  = string
+#     default = "10.0.0.0/16"
+# }
+
+# variable  "enable_dns_hostnames"{
+#     type = bool
+#     default = true
+# }
+
+# variable "vpc_tags" {
+#     type = map
+#     default = {}
+# }
+
+#  ##igw tags##
+# variable "igw_tags"{
+#     type = map
+#     default = {}
+# }
+
+
+
+
+
+
+# #public subnets####
+
+# variable "public_subnet_cidrs" {
+#     type = list
+#      validation {
+#     condition     = length(var.public_subnet_cidrs) ==2
+#     error_message = "please provide 2  valid public subnet cidr"
+#   }
+#     }
+
+
+# variable "public_subnet_cidrs_tags" {
+#     type = map 
+#     default = {}
+#     }
+
+#     variable "private_subnet_cidrs" {
+#     type = list
+#      validation {
+#     condition     = length(var.private_subnet_cidrs) ==2
+#     error_message = "please provide 2  valid private subnet cidr"
+#   }
+#     }
+
+
+# variable "private_subnet_cidrs_tags" {
+#     type = map 
+#     default = {}
+#     }
+    
+#     variable "database_subnet_cidrs" {
+#     type = list
+#      validation {
+#     condition     = length(var.database_subnet_cidrs) ==2
+#     error_message = "please provide 2  valid database subnet cidr"
+#   }
+#     }
+
+
+# variable "database_subnet_cidrs_tags" {
+#     type = map 
+#     default = {}
+#     }
+
+
+#     variable"database_subnet_group_tags"{
+#         type = map 
+#         default = {}
+#     }
+    
+
+#     variable "nat_gateway_tags" {
+#     type = map 
+#     default = {}
+#     }
+    
+
+
+#     variable "public_route_table_tags" {
+#     type = map 
+#     default = {}
+#     }
+
+
+#      variable "private_route_table_tags" {
+#     type = map 
+#     default = {}
+#     }
+
+
+#      variable "database_route_table_tags" {
+#     type = map 
+#     default = {}
+#     }
+
+
+
+#     ###peering####
+
+
+#     variable "is_peering_required"{
+#         type =bool 
+#         default = false
+#     }
+
+
+#     variable "acceptor_vpc_id" {
+#         type = string
+#         default = ""
+#     }
+
+
+#     variable "peering_tags"{
+#         type = map
+#         default = {}
+#     }
+
+
+
+
+#### Project ####
+variable "project_name"{
+    type = string
 }
 
-variable "environment" {
+variable "environment"{
     type = string
     default = "dev"
 }
 
-variable "common_tags" {
+variable "common_tags"{
     type = map
 }
 
-
-
+#### VPC ####
 variable "vpc_cidr" {
-    type  = string
+    type = string
     default = "10.0.0.0/16"
 }
 
-variable  "enable_dns_hostnames"{
+variable "enable_dns_hostnames" {
     type = bool
     default = true
 }
@@ -28,110 +169,97 @@ variable "vpc_tags" {
     default = {}
 }
 
- ##igw tags##
+#### IGW ####
 variable "igw_tags"{
     type = map
     default = {}
 }
 
-
-
-
-
-
-#public subnets####
-
+### Public Subnet ####
 variable "public_subnet_cidrs" {
     type = list
-     validation {
-    condition     = length(var.public_subnet_cidrs) ==2
-    error_message = "please provide 2  valid public subnet cidr"
-  }
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet CIDR"
     }
+}
 
-
-variable "public_subnet_cidrs_tags" {
-    type = map 
+variable "public_subnet_cidr_tags" {
+    type = map
     default = {}
-    }
+}
 
-    variable "private_subnet_cidrs" {
+
+### Private Subnet ####
+variable "private_subnet_cidrs" {
     type = list
-     validation {
-    condition     = length(var.private_subnet_cidrs) ==2
-    error_message = "please provide 2  valid private subnet cidr"
-  }
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid private subnet CIDR"
     }
+}
 
-
-variable "private_subnet_cidrs_tags" {
-    type = map 
+variable "private_subnet_cidr_tags" {
+    type = map
     default = {}
-    }
-    
-    variable "database_subnet_cidrs" {
+}
+
+
+### Database Subnet ####
+variable "database_subnet_cidrs" {
     type = list
-     validation {
-    condition     = length(var.database_subnet_cidrs) ==2
-    error_message = "please provide 2  valid database subnet cidr"
-  }
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid database subnet CIDR"
     }
+}
 
-
-variable "database_subnet_cidrs_tags" {
-    type = map 
+variable "database_subnet_cidr_tags" {
+    type = map
     default = {}
-    }
+}
 
-
-    variable"database_subnet_group_tags"{
-        type = map 
-        default = {}
-    }
-    
-
-    variable "nat_gateway_tags" {
-    type = map 
+variable "database_subnet_group_tags" {
+    type = map
     default = {}
-    }
-    
+}
 
-
-    variable "public_route_table_tags" {
-    type = map 
+#### Nat gateway ####
+variable "nat_gateway_tags" {
+    type = map
     default = {}
-    }
+}
 
-
-     variable "private_route_table_tags" {
-    type = map 
+#### Public Route table ####
+variable "public_route_table_tags" {
+    type = map
     default = {}
-    }
+}
 
-
-     variable "database_route_table_tags" {
-    type = map 
+#### Private Route table ####
+variable "private_route_table_tags" {
+    type = map
     default = {}
-    }
+}
 
+#### Database Route table ####
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
 
+#### Peering ####
+variable "is_peering_required" {
+  type = bool
+  default = false
+}
 
-    ###peering####
+variable "acceptor_vpc_id" {
+  type = string
+  default = ""
+}
 
-
-    variable "is_peering_required"{
-        type =bool 
-        default = false
-    }
-
-
-    variable "acceptor_vpc_id" {
-        type = string
-        default = ""
-    }
-
-
-    variable "peering_tags"{
-        type = map
-        default = {}
-    }
+variable "vpc_peering_tags" {
+  type = map
+  default = {}
+}
